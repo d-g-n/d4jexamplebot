fc38a343d2bfe97e92bd52100f2edf46c93d95f5
declan <declan.g.neilson@gmail.com>
2017-04-17 04:08:51 +0100

    Oops

--- src/main/java/com/github/decyg/Command.java
+++ src/main/java/com/github/decyg/Command.java
@@ -1,6 +1,5 @@
 package com.github.decyg;
 
-import com.arsenarsen.lavaplayerbridge.libraries.UnknownBindingException;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
 import java.util.List;
@@ -11,6 +10,6 @@ import java.util.List;
 public interface Command {
 
     // Interface for a command to be implemented in the command map
-    void runCommand(MessageReceivedEvent event, List<String> args) throws UnknownBindingException;
+    void runCommand(MessageReceivedEvent event, List<String> args);
 
 }
